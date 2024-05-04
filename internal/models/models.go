5e84c50c126f6d5597f425a17b46084a19b91a69
ArCaneSec <arcanebughunter@gmail.com>
2024-05-04 21:11:52 +0330

    indexes, constraints

--- internal/models/models.go
+++ internal/models/models.go
@@ -12,29 +12,38 @@ type SimpleModel struct {
 
 type Company struct {
 	SimpleModel
-	Name string
+	Name string `gorm:"uniqueIndex;not null;size:255;"`
 }
 
 type Email struct {
 	SimpleModel
-	Email     string
-	CompanyID uint
-	Company   Company
+	Email     string  `gorm:"uniqueIndex;not null;size:255;"`
+	CompanyID uint    `gorm:"index;not null;"`
+	Company   Company `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 type Domain struct {
 	SimpleModel
-	Domain       string
-	Methods      []Method    `gorm:"many2many:domain_method;"`
-	Parameters   []Parameter `gorm:"many2many:domain_parameter;"`
-	CompanyID    uint
-	Company      Company
+	Domain     string      `gorm:"uniqueIndex;not null;size:255;"`
+	Methods    []Method    `gorm:"many2many:domain_methods;"`
+	Parameters []Parameter `gorm:"many2many:domain_parameters;"`
+	CompanyID  uint        `gorm:"index;not null;"`
+	Company    Company     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 type Parameter struct {
 	SimpleModel
-	Parameter string
-	Domains   []Domain `gorm:"many2many:domain_parameter;"`
+	Parameter string   `gorm:"uniqueIndex;not null;size:255;"`
+	Domains   []Domain `gorm:"many2many:domain_parameters;"`
+}
+
+type Endpoint struct {
+	SimpleModel
+	Path       string      `gorm:"uniqueIndex:unique_path_param;not null;size:255;"`
+	Methods    []Method    `gorm:"many2many:endpoint_methods;"`
+	Parameters []Parameter `gorm:"many2many:endpoint_parameters;"`
+	DomainID   uint        `gorm:"uniqueIndex:unique_path_param;not null;"`
+	Domain     Domain      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 type Status struct {
@@ -45,13 +54,4 @@ type Status struct {
 type Method struct {
 	ID     uint `gorm:"primaryKey"`
 	Method string
-}
-
-type Endpoint struct {
-	SimpleModel
-	Path         string
-	Methods      []Method    `gorm:"many2many:endpoint_method;"`
-	Parameters   []Parameter `gorm:"many2many:endpoint_parameter;"`
-	DomainID     uint
-	Domain       Domain
 }
